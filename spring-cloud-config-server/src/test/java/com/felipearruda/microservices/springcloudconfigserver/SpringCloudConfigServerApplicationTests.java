660899bd2caa45490a88c301441563609f26fac5
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-08-02 21:24:29 -0300

    Criação do repositório Git Local

---
+++ spring-cloud-config-server/src/test/java/com/felipearruda/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipearruda.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
