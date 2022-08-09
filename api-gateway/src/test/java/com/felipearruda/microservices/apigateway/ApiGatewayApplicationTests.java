29536b2c9e8183255cb68472fe3119274e1b1c14
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-08-09 19:21:28 -0300

    Criada Api Gateway

---
+++ api-gateway/src/test/java/com/felipearruda/microservices/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipearruda.microservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
