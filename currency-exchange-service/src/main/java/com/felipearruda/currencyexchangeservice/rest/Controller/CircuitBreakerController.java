4b128133a3ac2862bc82f4b43f37e53924059ffd
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-08-10 22:55:06 -0300

    Utilizando o CircuitBreaker

--- currency-exchange-service/src/main/java/com/felipearruda/currencyexchangeservice/rest/Controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/felipearruda/currencyexchangeservice/rest/Controller/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.felipearruda.currencyexchangeservice.rest.Controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,7 +18,7 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
     public String sampleApli() {
         logger.info("Sample api call received");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
