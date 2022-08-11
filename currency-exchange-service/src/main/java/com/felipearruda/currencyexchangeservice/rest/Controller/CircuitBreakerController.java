43927479b1c29bbf97819b7a3d7eb98eed340ef4
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-08-10 23:05:17 -0300

    Configurando quantidade de requisições por periodo

--- currency-exchange-service/src/main/java/com/felipearruda/currencyexchangeservice/rest/Controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/felipearruda/currencyexchangeservice/rest/Controller/CircuitBreakerController.java
@@ -1,16 +1,10 @@
 package com.felipearruda.currencyexchangeservice.rest.Controller;
 
-import io.github.resilience4j.bulkhead.annotation.Bulkhead;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import javax.ws.rs.core.Response;
 
 
 @RestController
@@ -19,7 +13,7 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @Bulkhead(name ="sample-api")
+    @RateLimiter(name="default")
     public String sampleApli() {
         logger.info("Sample api call received");
         return "sample-api";
