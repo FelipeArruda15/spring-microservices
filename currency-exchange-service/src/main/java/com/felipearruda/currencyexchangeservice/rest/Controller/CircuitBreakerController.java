2a8387e701350a5510c6712b0ccffc600a011e52
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-08-10 23:02:52 -0300

    Configurando quantidade de requisições simultâneas

--- currency-exchange-service/src/main/java/com/felipearruda/currencyexchangeservice/rest/Controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/felipearruda/currencyexchangeservice/rest/Controller/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.felipearruda.currencyexchangeservice.rest.Controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
@@ -18,12 +19,10 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+    @Bulkhead(name ="sample-api")
     public String sampleApli() {
         logger.info("Sample api call received");
-        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
-				String.class);
-        return forEntity.getBody();
+        return "sample-api";
     }
 
     public String hardcodedResponse(Exception ex){
